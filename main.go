b5f071247f8e7ee95c3e87e5505a210c95863240
jayzone91 <register@j4yz0n3.de>
2025-06-02 11:51:48 +0200

    Update main.go

--- main.go
+++ main.go
@@ -1,3 +1,7 @@
 package main
 
-func main() {}
+func main() {
+	// TODO: Read Mitarbeiter from Homepage DB
+	// TODO: Create Template for Signature
+	// TODO: Generate Signatures from Template in separate Folders
+}
